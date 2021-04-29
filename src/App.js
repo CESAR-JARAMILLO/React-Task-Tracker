0cf650c76996ae310304a7b025601470abf4d1fc
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-29 09:41:38 -0700

    state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "kgku",
+        day: "jhkhh",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "kgkmku",
+        day: "jhkhh",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "kgjhnkjku",
+        day: "jhkhh",
+        reminder: true
+    },
+])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
