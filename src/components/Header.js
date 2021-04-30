9c50122fbda73efdbd2372926152028cb9f6a8d0
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-30 15:55:28 -0700

    toggle form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,11 @@ import PropTypes from 'prop-types'
 import Button from "./Button"
 
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("Click")
-    }
-
+const Header = ({ title, onAdd }) => {
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color="green" text="Hello" onClick={onClick} />
+            <Button color="green" text="Add" onClick={onAdd} />
         </header>
     )
 }
